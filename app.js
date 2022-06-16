80f74095f3d68e5b44eee275ce124c3b5024baf6
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-06-16 10:27:15 +0100

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('This code is in typescript');
+function add(n1, n2) {
+    // You can use this in Vanilla JavaScript
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Incorreect Input!');
+    }
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 8.87;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('This code is in typescript');
+function add(n1: number, n2: number) {
+    // You can use this in Vanilla JavaScript
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Incorreect Input!')
+    }
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 8.87;
+
+const result = add(number1, number2);
+console.log(result); 
